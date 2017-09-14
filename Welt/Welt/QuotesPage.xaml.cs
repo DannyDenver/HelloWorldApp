383a4d729807f267afeed053c5f77d06d3f32a27
Daniel Taylor <djt09d@fulbrightmail.org>
2017-09-14 09:56:51 -0600

    Add project files.

---
+++ Welt/Welt/QuotesPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Welt
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuotesPage : ContentPage
+    {
+        int index = 0; 
+        string[] quotes = new string[] { "You get smarter, when you eat brocolli.", "Brush your teeth twice a day", "Dont drink coffee past 4" };
+        public  QuotesPage()
+        {
+            InitializeComponent();
+            label.Text = "Howdy yall";
+        }
+
+        void Change_Quote(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            if(index == quotes.Length)
+            {
+                index = 0;
+                label.Text = quotes[index];
+            }
+            else
+            {
+                label.Text = quotes[index];
+                index = index + 1;
+            }
+        }
+
+    }
+}
